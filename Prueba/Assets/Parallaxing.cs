9c7084c899d7ef802479b79b06d375045654de93
Cr0wHogan <nicolaserrrea@hotmail.com>
2019-12-06 10:01:31 -0300

    hgjgfjf

---
+++ Prueba/Assets/Parallaxing.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{   public Transform[] backgrounds;    //ARREGLO DE TODOS LAS PARTES TRASERAS Y DELANTERAS QUE VAN A PARALLEXEARSE
+    private float[] parallaxScales;    //PROPORCIONES DE LA CAMARA QUE SE VAN A MOVER CON EL BACKGROUND
+    public float smoothing = 1f;            //QUE TAN FLUIDO VA A SER EL PARALLAX (SETEAR SIEMPRE EN 0)
+
+    private Transform cam;            //Referencia a la camara main transform
+    private Vector3 previosCamPos;    //La posicion de la camara en el frame previo
+
+    //Awake se llama antes del start (es para asignar variables como la cam) (referencias)
+    void Awake ()
+    {
+        cam = Camera.main.transform;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        previosCamPos = cam.position;
+
+        //ASIGNAR LA ESCALA CORRESPONDIENTE
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i=0; i<backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z*-1;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            //PARALLAX OPUESTO A EL MOVIMIENTO DE LA CAMARA PORQUE EL ULTIMO FRAME ES MULTIPLICADO POR LA ESCALA
+            float parallax = (previosCamPos.x - cam.position.x) * parallaxScales[i];
+
+
+            //SETEAR LA POS X EN LA MISMA POSICION PERO SUMANDOLE EL PARALLAX
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            //AJUSTAR LOS VECTORES DE LA CAMARA CON LA POSICION DE X
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        //SETEAR LA CAMARA PREVIA AL FINAL DEL FRAME
+        previosCamPos = cam.position;
+    }
+}
---
+++ Prueba/Assets/Tiling.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour
+{
+    public int offsetX = 2; //PARA NO TENER ERRORES
+
+    //PARA CHEQUEAR LOS BORDES SI NECESITAMOS
+    public bool hasARightBuddy = false;
+    public bool hasALeftBuddy = false;
+
+    public bool reverseScale = false; //PARA SABER SI EL OBJETO NO ESTA TILABLE
+
+    private float spriteWidth = 0f;  //PARA SABER LA ANCHURA DEL ELEMENTO
+    private Camera cam;
+    private Transform myTransform;
+
+    void Awake ()
+    {
+        cam = Camera.main;
+        myTransform = transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+        spriteWidth = sRenderer.sprite.bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si todavía necesita agrandarse, sino no hace nada
+        if (hasALeftBuddy == false || hasARightBuddy == false)
+        {
+            //calcular la extensión desde el centro de la cámara hacia el marco exterior
+            float camHorizontalExtend = cam.orthographicSize * Screen.width / Screen.height;
+
+            //calcular la camara que avanza y la que se retrasa (visualmente hablando)
+            float edgeVisiblePositionRight = (myTransform.position.x + spriteWidth / 2) - camHorizontalExtend;
+            float edgeVisiblePositionLeft = (myTransform.position.x - spriteWidth / 2 + camHorizontalExtend);
+
+            //si el margen de la cámara alcanza suelo creamos una porción de suelo
+            if (cam.transform.position.x >= edgeVisiblePositionRight - offsetX && hasARightBuddy == false)
+            {
+                MakeNewBuddy(1);
+                hasARightBuddy = true;
+            }
+            else if (cam.transform.position.x <= edgeVisiblePositionLeft + offsetX && hasALeftBuddy == false)
+            {
+                MakeNewBuddy(-1);
+                hasALeftBuddy = true;
+            }
+        }
+    }
+    //PARA CALCULAR LA NUEVA PORCION DE SUELO
+    void MakeNewBuddy(int rightOrLeft)
+    {
+        Vector3 newPosition = new Vector3(myTransform.position.x + spriteWidth * rightOrLeft, myTransform.position.y, myTransform.position.z);
+        //instanciar nueva porcion en una variable
+        Transform newBuddy = Instantiate(myTransform, newPosition,myTransform.rotation) as Transform;
+        if (reverseScale == true)
+        {
+            newBuddy.localScale = new Vector3(newBuddy.localScale.x*-1,newBuddy.localScale.y,newBuddy.localScale.z);
+        }
+        newBuddy.parent = myTransform.parent;
+        if (rightOrLeft > 0)
+        {
+            newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+        }
+        else
+        {
+            newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+        }
+    }
+}
